bfd44357dba6b21b449d2cfe39ce79bf461b504e
softmarshmallow <woojoo@softmarshmallow.com>
2022-07-01 15:41:23 +0900

    init jsx namer package

---
+++ naming-jsx-engine/sources/github_styled_components.py
@@ -0,0 +1,8 @@
+from github import Github
+g = Github("access_token")
+
+
+# get all repositories with dependency "styled-components" - which the id is "UGFja2FnZS01MDYyMTAyMzk="
+# https://github.com/styled-components/styled-components/network/dependents?package_id=UGFja2FnZS01MDYyMTAyMzk%3D
+
+
