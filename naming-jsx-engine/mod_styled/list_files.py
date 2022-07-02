d68e03fc5bc8491eef6340fb1a60db619c011884
softmarshmallow <woojoo@softmarshmallow.com>
2022-07-02 22:53:58 +0900

    add non regex mode

--- naming-jsx-engine/mod_styled/list_files.py
+++ naming-jsx-engine/mod_styled/list_files.py
@@ -3,7 +3,7 @@ import glob
 import utils_ast
 
 
-EXTS = ['.jsx', '.js', '.tsx', '.ts']
+EXTS = ('jsx', 'js', 'tsx', 'ts')
 
 
 def list_styled_components_files(project_path):
@@ -11,17 +11,42 @@ def list_styled_components_files(project_path):
     find files uses styled-components by searching for the import statement
     -> import styled from "styled-components";
     '''
+    # match one of the extensions
+    files = []
+    for ext in EXTS:
+        files.extend(
+            glob.glob(f'**/*.{ext}', root_dir=project_path, recursive=True))
 
-    files = glob.glob(
-        project_path + '**/*.{}'.format(''.join(EXTS)), recursive=True)
-    print(len(files))
     # find files uses styled-components by searching for the import statement
     # -> import styled from "styled-components";
     styled_components_files = []
     for f in files:
         path = os.path.join(project_path, f)
-        if utils_ast.contains_js_import_statement(path, 'styled', 'styled-components') \
-                or utils_ast.contains_js_import_statement(path, 'styled', '@emotion/styled'):
-            styled_components_files.append(path)
-
+        try:
+            with open(path, 'r') as fp:
+                txt = fp.read()
+                if utils_ast.contains_js_import_statement(txt, 'styled', 'styled-components', regex=False) or utils_ast.contains_js_import_statement(txt, 'styled', '@emotion/styled', regex=False):
+                    styled_components_files.append(path)
+        except Exception:
+            pass
     return styled_components_files
+
+
+if __name__ == '__main__':
+    print(utils_ast.contains_js_import_statement(
+        '''import styled from "styled-components";
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;''', 'styled', 'styled-components'))
+    # print(list_styled_components_files(
+    #     '/Volumes/Data/public-github-archives/archives/MileneGJ/projeto13-mywallet-front'))
--- naming-jsx-engine/utils_ast/re_import_statement.py
+++ naming-jsx-engine/utils_ast/re_import_statement.py
@@ -3,19 +3,46 @@
 #
 import re
 
+import_statements_regex = r'''import([ \n\t]*(?:[^ \n\t\{\}]+[ \n\t]*,?)?(?:[ \n\t]*\{(?:[ \n\t]*[^ \n\t"'\{\}]+[ \n\t]*,?)+\})?[ \n\t]*)from[ \n\t]*(['"])([^'"\n]+)(?:['"])'''
 
-def contains_js_import_statement(source: str, _import: str, _from: str):
-    # https://stackoverflow.com/a/69867053/5463235
-    import_statements_regex = r'''import([ \n\t]*(?:[^ \n\t\{\}]+[ \n\t]*,?)?(?:[ \n\t]*\{(?:[ \n\t]*[^ \n\t"'\{\}]+[ \n\t]*,?)+\})?[ \n\t]*)from[ \n\t]*(['"])([^'"\n]+)(?:['"])'''
-    # $1 imported variables          - must contain "React"
-    # $2 quotes used for the import  -
-    # $3 import path                 - must be '"react"' or "'react'"
 
-    variables_imported = re.findall(import_statements_regex, source)
-    for variables, quotes, import_path in variables_imported:
-        if _import in variables:
-            print(import_path)
-            if import_path == f'{_from}' or import_path == f"{_from}":
-                return True
-
+def test(txt: str, _import: str, _from: str,):
+    try:
+        # https://stackoverflow.com/a/69867053/5463235
+        variables_imported = re.findall(import_statements_regex, txt)
+        for variables, quotes, import_path in variables_imported:
+            if _import in variables:
+                if import_path == _from:
+                    return True
+            pass
+    except Exception:
+        return False
     return False
+
+
+def contains_js_import_statement(source: str, _import: str, _from: str, by_lines=True, regex=True):
+    if regex:
+        if by_lines:
+            for line in source.split('\n'):
+                if test(line, _import, _from):
+                    return True
+        else:
+            return test(source, _import, _from)
+        return False
+    else:
+        return f'import {_import} from \'{_from}\'' in source or f'import {_import} from \"{_from}\"' in source
+
+
+if __name__ == '__main__':
+    _ = contains_js_import_statement(
+        '''
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};''', 'styled', 'styled-components', regex=False)
+    print(_)
