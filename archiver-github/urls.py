89a8d34f2959447f677afaac7a78820e79f914a3
UZU, J <woojoo@softmarshmallow.com>
2022-07-09 03:59:05 +0900

    init urls.py

---
+++ archiver-github/urls.py
@@ -0,0 +1,11 @@
+import click
+
+
+@click.command()
+@click.option('--o', default=None, help='outfile path (usually urls.txt)')
+def main():
+    pass
+    return f'https://github.com/{fullname}/archive/master.tar.gz'
+
+if __name__ == "__main__":
+    pass
