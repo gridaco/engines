791378784fe61749262db196e9bd7d3500d30f48
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-08 10:44:58 +0900

    vaegen

--- fig2fig/vaegen/train.py
+++ fig2fig/vaegen/train.py
@@ -5,6 +5,25 @@ from torch.utils.data import DataLoader
 from dataset import FigmaNodesDataset
 import numpy as np
 from tqdm import tqdm
+from torch.utils.data import Dataset
+
+class TensorDataset(Dataset):
+    def __init__(self, saved_tensors_file):
+        self.data = torch.load(saved_tensors_file)
+        self.max_length = max([t.shape[0] for t, _, _ in self.data])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        tensor_features, root_type, dimensions = self.data[idx]
+        padded_tensor = self.pad_tensor(tensor_features, self.max_length)
+        return padded_tensor, root_type, dimensions
+
+    def pad_tensor(self, tensor, target_length):
+        pad_size = target_length - tensor.size(0)
+        return torch.cat((tensor, torch.zeros(pad_size, tensor.size(1))), dim=0)
+
 
 # VAE Model
 class VAE(nn.Module):
@@ -51,8 +70,8 @@ def train(model, dataloader, device, optimizer, epochs):
         progress_bar = tqdm(enumerate(dataloader), total=len(dataloader), desc=f"Epoch {epoch + 1}/{epochs}", unit="batch")
         for batch_idx, (data, _type, wh) in progress_bar:
             data = data.to(device).float()
-            _type = torch.tensor(_type, device=device, dtype=torch.float32).squeeze(1)  # Convert the _type tuple to a tensor
-            wh = torch.tensor(wh, device=device, dtype=torch.float32).squeeze(1)  # Convert the wh tuple to a tensor and fix the dimensions
+            _type = _type.to(device=device, dtype=torch.float32).squeeze(1)  # Convert the _type tuple to a tensor
+            wh = wh.to(device=device, dtype=torch.float32).squeeze(1)  # Convert the wh tuple to a tensor and fix the dimensions
             optimizer.zero_grad()
             recon_batch, mu, log_var = model(data, _type, wh)
             loss = vae_loss(recon_batch, data, mu, log_var)
@@ -60,6 +79,14 @@ def train(model, dataloader, device, optimizer, epochs):
             train_loss += loss.item()
             optimizer.step()
             progress_bar.set_description(f"Epoch {epoch + 1}/{epochs}, Loss: {train_loss / (batch_idx + 1)}")
+
+
+def custom_collate(batch):
+    data = torch.stack([item[0] for item in batch], dim=0)
+    _type = torch.tensor([item[1] for item in batch], dtype=torch.float32).unsqueeze(1)
+    wh = torch.tensor([item[2] for item in batch], dtype=torch.float32).unsqueeze(1)
+    return data, _type, wh
+
 
 # Configuration
 hidden_dim = 512
@@ -70,12 +97,12 @@ learning_rate = 1e-3
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def main():
-    # Load dataset
-    db_path = "path/to/your/database"  # Replace this with the path to your dataset
-    dataset = FigmaNodesDataset(db_path)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    # Load saved tensors
+    saved_tensors_file = "./checkpoints/nodes-100-100.pth"  # Replace this with the path to your saved tensors file
+    dataset = TensorDataset(saved_tensors_file)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate)
 
-    input_dim = dataset.get_input_dim()
+    input_dim = dataset[0][0].shape[1]
 
     # Initialize and train VAE
     model = VAE(input_dim, hidden_dim, latent_dim).to(device)
@@ -85,6 +112,7 @@ def main():
     # Save the model
     torch.save(model.state_dict(), "vae_model.pth")
     print("Model saved to vae_model.pth")
+
 
 if __name__ == "__main__":
     main()
